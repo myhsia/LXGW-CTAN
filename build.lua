e82295bd348e8dc68174fe56abef724459baaefb
myhsia <myhsia@outlook.com>
2025-11-29 16:18:44 +0800

    Demo added.

--- build.lua
+++ build.lua
@@ -7,9 +7,9 @@
              Do Check Before Push
 --]==========================================]--
 
-module              = "lxgw"
+module              = "LXGW-fonts"
 version             = "v1.521"
-date                = "2025-11-28"
+date                = "2025-11-29"
 maintainer          = "Mingyu Xia"
 uploader            = "Mingyu Xia"
 maintainid          = "myhsia"
@@ -28,6 +28,9 @@ description         = "The LXGW Font Family provides an unprofessional open-sour
 ctanzip             = module
 excludefiles        = {"*~"}
 textfiles           = {"*.md", "LICENSE", "*.lua", "*.ttf"}
+typesetfiles        = {"*.tex"}
+typesetexe          = "latexmk -xelatex"
+typesetruns         = 1
 unpacksuppfiles     = {"*.txt"}
 
 uploadconfig  = {
@@ -48,11 +51,41 @@ uploadconfig  = {
   development  = "https://github.com/" .. maintainid,
   update       = false
 }
+function update_tag(file, content, tagname, tagdate)
+  tagname = version
+  tagdate = date
+  if string.match(file, "%.dtx$") or string.match(file, "%.tex$") then
+    content = string.gsub(content,
+      "\\date{Released %d+%-%d+%-%d+\\quad \\texttt{v([%d%.A-Z]+)}}",
+      "\\date{Released " .. tagdate .. "\\quad \\texttt{" .. tagname .. "}}")
+  end
+  return content
+end
+
+--[== "Hacks" to `l3build` | Do not Modify ==]--
 
 function docinit_hook()
   cp("fetch.txt",  maindir, unpackdir)
   run(unpackdir, "wget -i fetch.txt")
   run(unpackdir, "unzip LxgwMarkerGothic-v1.003.zip")
   run(unpackdir, "mv ./LxgwMarkerGothic-v1.003/fonts/ttf/*.ttf ./")
+  run(unpackdir, "mv ./Xiaolai-Regular.ttf ./LXGWXiaolai-Regular.ttf")
+  run(unpackdir, "mv ./Yozai-Regular.ttf ./LXGWYozai-Regular.ttf")
+  run(unpackdir, "mv ./Yozai-Medium.ttf ./LXGWYozai-Medium.ttf")
   return 0
+end
+function tex(file,dir,cmd)
+  dir = dir or "."
+  cmd = cmd or typesetexe
+  if os.getenv("WINDIR") ~= nil or os.getenv("COMSPEC") ~= nil then
+    upretex_aux = "-usepretex=\"" .. typesetcmds .. "\""
+    makeidx_aux = "-e \"$makeindex=q/makeindex -s " .. indexstyle .. " %O %S/\""
+    sandbox_aux = "set \"TEXINPUTS=../unpacked;%TEXINPUTS%;\" &&"
+  else
+    upretex_aux = "-usepretex=\'" .. typesetcmds .. "\'"
+    makeidx_aux = "-e \'$makeindex=q/makeindex -s " .. indexstyle .. " %O %S/\'"
+    sandbox_aux = "TEXINPUTS=\"../unpacked:$(kpsewhich -var-value=TEXINPUTS):\""
+  end
+  return run(dir, sandbox_aux .. " " .. cmd         .. " " ..
+                  upretex_aux .. " " .. makeidx_aux .. " " .. file)
 end
