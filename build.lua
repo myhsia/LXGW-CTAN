3978c693b6f200bb2106300c66d942de384ad6df
myhsia <myhsia@outlook.com>
2026-01-13 21:31:31 +0800

    ISSUE #2 Fixed

--- build.lua
+++ build.lua
@@ -8,8 +8,8 @@
 --]==========================================]--
 
 module              = "lxgw-fonts"
-version             = "v1.521J"
-date                = "2026-01-01"
+version             = "v1.521K"
+date                = "2026-01-13"
 maintainer          = "Mingyu Xia"
 uploader            = "Mingyu Xia"
 maintainid          = "myhsia"
