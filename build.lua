e0c029fa385516900009b604e3fe28e2e09c91f5
Mingyu Hsia <myhsia@outlook.com>
2026-05-14 15:45:11 +0800

    Enable regex test

--- build.lua
+++ build.lua
@@ -23,10 +23,12 @@ description         = "The `LXGW` Font Family provides an open-source CJK font f
          Do not Modify Unless Necessary
 --]==========================================]--
 
+checkengines        = {"xetex", "uptex", "luatex"}
 ctanzip             = module
 excludefiles        = {"*~"}
-installfiles        = {"*.def", "*.tex", "*.spa"}
-textfiles           = {"README.md", "LICENSE", "*.lua", "*.ttf"}
+installfiles        = {"*.def", "*.tex", "*.spa", "*.ttf", "*.otf"}
+sourcefiles         = {"*.dtx", "*.ins", "*.ttf", "*.otf"}
+textfiles           = {"README.md", "*.csv", "LICENSE", "*.lua"}
 typesetexe          = "latexmk -pdfxe"
 typesetruns         = 1
 unpacksuppfiles     = {"*.txt"}
@@ -68,12 +70,74 @@ function update_tag(file, content, tagname, tagdate)
   end
   return content
 end
+
+function bundleunpack(sourcedirs, sources)
+  errorlevel = run(currentdir, "xargs -n 1 curl -O -L < lxgw-fetch.csv")
+  if errorlevel ~=0 then
+    return errorlevel
+  end
+  errorlevel = run(currentdir, "unzip ZhuqueFangsong-v0.212.zip")
+  if errorlevel ~=0 then
+    return errorlevel
+  end
+  errorlevel = rm(currentdir, "ZhuqueFangsong-v0.212.zip")
+  if errorlevel ~=0 then
+    return errorlevel
+  end
+  errorlevel = ren(currentdir, "ZhuqueFangsong-Regular.ttf", "LXGWZhuqueFangsong-Regular.ttf")
+  if errorlevel ~=0 then
+    return errorlevel
+  end
+  local errorlevel = mkdir(localdir)
+  if errorlevel ~=0 then
+    return errorlevel
+  end
+  errorlevel = cleandir(unpackdir)
+  if errorlevel ~=0 then
+    return errorlevel
+  end
+  for _,i in ipairs(sourcedirs or {sourcefiledir}) do
+    for _,j in ipairs(sources or {sourcefiles}) do
+      for _,k in ipairs(j) do
+        errorlevel = cp(k, i, unpackdir)
+        if errorlevel ~=0 then
+          return errorlevel
+        end
+      end
+    end
+  end
+  for _,i in ipairs(unpacksuppfiles) do
+    errorlevel = cp(i, supportdir, localdir)
+    if errorlevel ~=0 then
+      return errorlevel
+    end
+  end
+  local popen = io.popen
+  for _,i in ipairs(unpackfiles) do
+    for _,p in ipairs(tree(unpackdir, i)) do
+      local path, name = splitpath(p.src)
+      local localdir = abspath(localdir)
+      local success = assert(popen(
+        "cd " .. unpackdir .. "/" .. path .. os_concat ..
+        os_setenv .. " TEXINPUTS=." .. os_pathsep
+          .. localdir .. (unpacksearch and os_pathsep or "") ..
+        os_concat  ..
+        os_setenv .. " LUAINPUTS=." .. os_pathsep
+          .. localdir .. (unpacksearch and os_pathsep or "") ..
+        os_concat ..
+        unpackexe .. " " .. unpackopts .. " " .. name
+          .. (options["quiet"] and (" > " .. os_null) or ""),
+        "w"
+      ):write(string.rep("y\n", 300))):close()
+      if not success then
+        return 1
+      end
+    end
+  end
+  return 0
+end
+
 function docinit_hook()
-  run(unpackdir, "xargs -n 1 curl -O -L < lxgw-fetch.csv")
-  run(unpackdir, "unzip ZhuqueFangsong-v0.212.zip")
-  ren(unpackdir, "ZhuqueFangsong-Regular.ttf", "LXGWZhuqueFangsong-Regular.ttf")
-  run(unpackdir, "xetex ctex-makespa-lxgw.tex")
-  cp("*.ttf", unpackdir, typesetdir)
   cp(ctanreadme, unpackdir, currentdir)
   return 0
 end
